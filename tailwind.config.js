b9f1bc831e490f37c9637fd726348af22df62742
Adrian Steffan <adrian.steffan@hotmail.de>
2025-03-02 19:37:56 +0100

    remove important property for tailwind migration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  important: true, // needed due to tailwind injection order when importing reactive
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   plugins: [require('@tailwindcss/typography') ],
 };
